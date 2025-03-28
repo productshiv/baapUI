45e08cd0226cd6b58e902c3f64d39f6ffa76073b
productshiv <shivam.srivastava@tatum.io>
2025-03-29 02:40:46 +0530

    fix: remove unused styles from App.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { ButtonShowcase } from './src/screens/ButtonShowcase';
 
 export default function App() {
   return <ButtonShowcase />;
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-}); 
+} 
