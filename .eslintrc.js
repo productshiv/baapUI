bb84208bda957c20bf16338cc74c5d7ed7cf7f59
productshiv <shivam.srivastava@tatum.io>
2025-04-04 11:55:06 +0530

    fix: simplify ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,20 @@
 module.exports = {
-  root: true,
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react-native/all',
-    'prettier',
+    'plugin:react-hooks/recommended'
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react', 'react-native'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  rules: {
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'react-native/no-inline-styles': 'warn',
-  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
-  env: {
-    'react-native/react-native': true,
-  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn'
+  }
 };
