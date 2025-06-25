bcf06c1d515e5aa4cdfca346f93109346161e2be
productshiv <shivam.srivastava@tatum.io>
2025-06-25 23:34:35 +0530

    Tests

--- jest.config.js
+++ jest.config.js
@@ -24,7 +24,7 @@ module.exports = {
     '!src/**/*.stories.{ts,tsx}',
     '!src/**/index.ts',
   ],
-  moduleNameMapping: {
+  moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
